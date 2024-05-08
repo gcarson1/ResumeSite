c855fb58494d0cfc59e0d2d18edf119d20fb85ef
gcarson1 <gcarson1@vols.utk.edu>
2024-05-08 01:36:57 -0400

    rework

--- pages/index.js
+++ pages/index.js
@@ -1,162 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+
 export default function Home() {
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>Gabriel Carson</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          <a href="https://nextjs.org">My Resume</a>
-        </h1>
-
-        <p className={styles.description}>
-          I program in: <code>C++ C JS Python SQL</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-      </div>
-      <div className={styles.home}>
-        <h1 className={styles.title}>
-            <a href="https://nextjs.org">Home</a>
-        </h1>
-      </div>
-      <div className={styles.about}>
-        <h1 className={styles.title}>
-            <a href="https://nextjs.org">About</a>
-        </h1>
-      </div>
-      <div className={styles.resume}>
-        <h1 className={styles.title}>
-            <a href="https://nextjs.org">Resume</a>
-        </h1>
-      </div>
-      <div className={styles.projects}>
-        <h1 className={styles.title}>
-            <a href="https://nextjs.org">Projects</a>
-        </h1>
-      </div>
-      <div className={styles.activities}>
-        <h1 className={styles.title}>
-            <a href="https://nextjs.org">Activities</a>
-        </h1>
-      </div>
-      <div className={styles.contact}>
-        <h1 className={styles.title}>
-            <a href="https://nextjs.org">Contact</a>
-        </h1>
-      </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          background: #2f2519;
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #2f2519;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          background-color: #4A3F35;
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
