81e386808ba41216d62c1ae165b144dae38f2552
gcarson1 <gcarson1@vols.utk.edu>
2024-05-09 02:19:40 -0400

    navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,14 +5,13 @@ import styles from '../styles/Home.module.css';
 
 const Navbar = () => {
   return (
-    <div className="nav-container">
+    <div className={styles.navContainer}>
       <div>
-        <Link className={styles.description} href="/">
-          Gabe's Portfolio 
-        </Link>
-      </div>
-      <div>
-        <Link href='/' className={styles.tab}> Resume</Link>
+        <Link className={styles.logo } href="/">Gabriel</Link>
+        <Link className={styles.ctaBtn } href="/">Home</Link>
+        <Link href='/' className={styles.ctaBtn}>Resume</Link>
+        <Link href='/' className={styles.ctaBtn}>Projects</Link>
+        <Link href='/' className={styles.ctaBtn}>Contact</Link>
 
       </div>
     </div>
