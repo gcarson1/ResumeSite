c8886f8b6fc13a4ed357b73cf3ea22e4ef1af09b
gcarson1 <gcarson1@vols.utk.edu>
2024-05-17 17:36:06 -0400

    hover color change navbar

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Gabriel Carson</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon1.ico" />
       </Head>
 
       <a href="/" className={styles.title}>Gabriel</a>
