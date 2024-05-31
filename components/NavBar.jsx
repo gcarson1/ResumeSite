3ad0a9e6421e721e506470902dfc3a9fdef8e02d
gcarson1 <gcarson1@vols.utk.edu>
2024-05-31 18:44:37 -0400

    Fixed resizing issues/responsive Nav Bar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,15 +1,39 @@
-import Hamburger from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleResize = () => {
+    if (window.innerWidth > 900 && isOpen) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsOpen(false);
+    };
+    router.events.on('routeChangeStart', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [router]);
 
   return (
     <nav className={styles.navContainer}>
@@ -18,17 +42,16 @@ const Navbar = () => {
           <Image className={styles.menuIcon} src='menu-burger-horizontal-svgrepo-com.svg' width={30} height={30} alt="Menu"/>
         </div>
         <div className={`${isOpen ? styles.showMenu : ''} ${isOpen ? styles.menuItemsVisible : styles.menuItems}`}>
-          <Link class={styles.navButton} href="../" onClick={toggleMenu}>Gabriel</Link>
+          <Link className={styles.navButton} href="../" >Gabriel</Link>
           <a> </a>
-          <Link class={styles.navButton} href="../resume" onClick={toggleMenu}>Resume</Link>
+          <Link className={styles.navButton} href="../resume" >Resume</Link>
           <a> </a>
-          <Link class={styles.navButton} href="../projects" onClick={toggleMenu}>Projects</Link>
+          <Link className={styles.navButton} href="../projects" >Projects</Link>
           <a> </a>
-          <Link class={styles.navButton} href="../contact" onClick={toggleMenu}>Contact</Link>
+          <Link className={styles.navButton} href="../contact" >Contact</Link>
           <a> </a>
-          <Link class={styles.navButton} href="../hobbies" onClick={toggleMenu}>Hobbies</Link>
+          <Link className={styles.navButton} href="../hobbies" >Hobbies</Link>
         </div>
-      
       </div>
     </nav>
   );
