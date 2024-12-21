c0b334621b6ef61b2e2870df7c97ac1cb915294a
gcarson1 <gcarson1@vols.utk.edu>
2024-12-21 18:53:59 -0500

    i understand it now

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,23 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     
-    <div className={styles.container}>
+    <div>
 
       <Head>
         <title>Gabriel Carson</title>
         <link rel="icon" href="/favicon1.ico" />
       </Head>
 
+      <div className={styles.home}>
 
-     <div>
-      <a href="/" className={styles.title}>Gabriel...</a>
-     </div>
+      <div>
+        <a href="/" className={styles.title}>Gabriel...</a>
+      </div>
 
-     <div>
-      <a className={styles.description}>I am a Computer Science student at The University of Tennessee-Knoxville graduating in December 2025. I have professional experience in Cloud Development and Data Governance. I also enjoy digital audio software stemming from my hobby of music production. I like the outdoors, basketball, cooking, and obviously video games.</a>
+      <div>
+        <a className={styles.description}>I am a Computer Science student at The University of Tennessee-Knoxville graduating in December 2025. I have professional experience in Cloud Development and Data Governance. I also enjoy digital audio software stemming from my hobby of music production. I like the outdoors, basketball, cooking, and obviously video games.</a>
+      </div>
+
      </div>
 
      <div>
